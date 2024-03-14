d9ed82fa6b35f42dc8ee045bb114486af5f28bff
Nikey <160123565+NikiValle@users.noreply.github.com>
2024-03-14 11:50:12 +0100

    Add files via upload

--- ArchivioNomi.java
+++ ArchivioNomi.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
-public class ArchivioNomi{
-    public static void main(String[]args){
+public class ArchivioNomi {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String [] bombe = new String [100];
+        String[] bombe = new String[100];
         int scelta;
-        int conta =-1;
+        int conta = -1;
+        String cerca;
         do {
-            conta = conta+1;
+            conta = conta + 1;
             System.out.println("Scegli un'opzione");
             System.out.println("1 - Aggiunta di un nome\n" +
                     "2 - Cancellazione del singolo nome\n" +
@@ -20,8 +21,22 @@ public class ArchivioNomi{
             scelta = in.nextInt();
             switch (scelta) {
                 case 1:
-
+                    System.out.println("Inserisci il nome");
+                    bombe[conta] = in.next();
+                    break;
+                case 2:
+                    System.out.println("Scegli il nome da cancellare");
+                    cerca = in.next();
+                    for (int i = 0; i < conta; i++) {
+                        if (bombe[i].equalsIgnoreCase(cerca)) {
+                            for (int j = i; j < conta; j++) {
+                                bombe[j] = bombe[j + 1];
+                            }
+                            conta = conta - 1;
+                            break;
+                        }
+                    }
             }
-        }while (scelta!=0);
+        } while (scelta != 0);
     }
 }
