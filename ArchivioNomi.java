b90492f21416c6944a3d5345ee15858e3ccba71e
Nikey <160123565+NikiValle@users.noreply.github.com>
2024-03-15 12:43:56 +0100

    Add files via upload

--- ArchivioNomi.java
+++ ArchivioNomi.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 public class ArchivioNomi {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] bombe = new String[100];
+        String[] strings = new String[100];
         int scelta;
         int conta = -1;
-        String cerca;
+        int ripete =0;
         do {
             conta = conta + 1;
             System.out.println("Scegli un'opzione");
@@ -21,25 +21,62 @@ public class ArchivioNomi {
             scelta = in.nextInt();
             switch (scelta) {
                 case 1:
-                    System.out.println("Inserisci il nome");
-                    bombe[conta] = in.next();
+                    Aggiunta(strings, conta, in);
                     break;
                 case 2:
-                    System.out.println("Scegli il nome da cancellare");
-                    cerca = in.next();
-                    for (int i = 0; i < conta; i++) {
-                        if (bombe[i].equalsIgnoreCase(cerca)) {
-                            for (int j = i; j < conta; j++) {
-                                bombe[j] = bombe[j + 1];
-                            }
-                            conta = conta - 1;
-                            break;
-                        }
-                    }
+                    Cancella(strings, conta);
                     break;
                 case 3:
-                    
+                    if(Ricerca(strings, conta, in)){
+                        System.out.println("Il nome è presente");
+                    }
+                    else{
+                        System.out.println("Il nome non è presente");
+                    }
+                    break;
+                case 4:
+                    Duplicati(strings, conta);
+                    break;
+                case 5:
+
             }
         } while (scelta != 0);
+    }
+    private static void Aggiunta (String [] a, int conta, Scanner in){
+        System.out.println("Inserisci il nome");
+        a[conta] = in.next();
+    }
+    private static void Cancella (String [] a, int conta){
+        for (int i = 0; i < conta; i++) {
+            a[i]=a[i+1];
+        }
+        conta = conta - 1;
+    }
+    private static boolean Ricerca(String[] a, int conta, Scanner in){
+        String cerca;
+        boolean trovato =false;
+        System.out.println("Inserisci un nome");
+        cerca = in.next();
+        for(int i =0;i<conta;i++){
+            if(a[i].equalsIgnoreCase(cerca)){
+                trovato = true;
+                break;
+            }
+        }
+        return trovato;
+    }
+    private static void Duplicati(String [] a, int conta){
+        int ripete =0;
+        for(int i =0;i<conta;i++){
+            for(int j =i;j<conta;j++){
+                if(a[i].equalsIgnoreCase(a[j])){
+                    ripete=ripete+1;
+                }
+            }
+            if(ripete>1){
+                System.out.println("Il nome "+a[i]+" si ripete "+ripete+" volte");
+            }
+            ripete=0;
+        }
     }
 }
